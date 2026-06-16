3d61623cdfe5d6f9a637c7d630f550c859a48acf
JWei <jay0935199@gmail.com>
2026-06-16 11:50:30 +0800

    feat: add ESLint LSP configuration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,6 +6,34 @@ return {
         intelephense = {
           root_markers = { "composer.json", ".git" },
         },
+        eslint = {
+          root_dir = function(fname)
+            local util = require("lspconfig.util")
+            return util.root_pattern(
+              "eslint.config.js",
+              "eslint.config.cjs",
+              "eslint.config.mjs",
+              ".eslintrc",
+              ".eslintrc.js",
+              ".eslintrc.cjs",
+              ".eslintrc.json",
+              ".eslintrc.yml",
+              ".eslintrc.yaml",
+              "package.json"
+            )(fname) or util.find_git_ancestor(fname)
+          end,
+
+          settings = {
+            eslint = {
+              experimental = {
+                useFlatConfig = false,
+              },
+              workingDirectory = {
+                mode = "location",
+              },
+            },
+          },
+        },
       },
     },
   },
